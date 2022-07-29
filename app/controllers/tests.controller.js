1b2736fa5bb8d8a063c6a16e2585141015ab5b3b
theHirsch <max@rose-niestetal.de>
2022-07-29 15:21:04 +0200

    commit import

---
+++ app/controllers/tests.controller.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+const Test = db.tests;
+const Op = db.Sequelize.Op;
+// Create and Save a new Test
+exports.create = (req, res) => {
+  
+};
+// Retrieve all Tests from the database.
+exports.findAll = (req, res) => {
+  
+};
+// Find a single Test with an id
+exports.findOne = (req, res) => {
+  
+};
+// Update a Test by the id in the request
+exports.update = (req, res) => {
+  
+};
+// Delete a Test with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+// Delete all Tests from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+// Find all published Tests
+exports.findAllPublished = (req, res) => {
+  
+};
