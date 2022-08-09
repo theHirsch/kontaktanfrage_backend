eb6c4e408a85b575af68fb9b044b402c39e7dea9
theHirsch <max@rose-niestetal.de>
2022-08-09 20:31:05 +0200

    bodyparser

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // simple test txt
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Max' App" });
